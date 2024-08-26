20ea772964c827c364f0e3cbf25a2f17c5c23d2d
AIMAN BIN AHMAD ZAINULKAMAL <aimanjay1@gmail.com>
2024-08-26 14:38:57 +0800

    Contact part
    Aku dh buat contact punya part, tpi sikit je, yg belah atas.

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,12 +4,28 @@ export default function Contact() {
   return (
     <main>
       <Header />
-      <div className="flex items-center bg-none flex-col w-full p-16">
-        <h1 className="text-9xl">Contact Us</h1>
-        <h4 className="text-2xl">Through</h4>
-        <div className="bg-white">
-          <div className="border-dashed border-black m-10 p-10">
-            <h5 className="text-xl">Send us an email at</h5>
+      <div className="bg-none w-full p-16">
+        <div className="flex-col">
+          <h1 className="text-9xl text-center">Contact Us</h1>
+          <h4 className="text-2xl text-center">Through</h4>
+        </div>
+
+        <div className="bg-white flex items-center justify-center w-25 h- mx-40 mt-20 ">
+          <div className="border border-black border-solid rounded-xl my-10">
+            <div className="flex">
+              <div className="m-10 p-10">
+                <h5 className="text-xl">Send us an email at</h5>
+              </div>
+              <div className="m-10 p-10">
+                <h5 className="text-xl text-end">deta@gmail.com</h5>
+              </div>
+            </div>
+
+            <div className="flex justify-center">
+              <button className="px-40 py-2 mb-10 bg-black text-white rounded-lg">
+                Send a Message Now!
+              </button>
+            </div>
           </div>
         </div>
       </div>
