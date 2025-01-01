fdd5c3b39c6fadf0afe676f27cc75da2d269cd56
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-01-01 21:46:22 +0800

    Changed Hierarchy structure to Community managers

--- app/organisation/page.jsx
+++ app/organisation/page.jsx
@@ -14,29 +14,27 @@ export default function Organisation() {
 
         <div className="mx-auto container p-4 space-y-8">
           <div className="flex flex-col bg-orange-100 p-2 md:p-10 rounded-lg">
-            <div className="text-xl text-center mb-4 lg:text-4xl"> The Mainboards </div>
-            <div className="grid grid-cols-3 w-full gap-4">
-              <OrganisationDisplay
-                position="President"
-                name="Hakim"
-                linkedin="https://www.linkedin.com/in/hakim-nazri/?lipi=urn%3Ali%3Apage%3Ad_flagship3_people_connections%3BXAF8PsjNRzekGbj3gerkyQ%3D%3D"
-                imageSrc="https://media.licdn.com/dms/image/v2/C5603AQGFQo-iw7WstQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1659781254837?e=1730937600&v=beta&t=qd0Po2EpgV4IXUOoWxb7o3N2AFTMLVZEFgRtTay86SM"
-              />
-              <OrganisationDisplay position="Vice President" name="Luqman" />
-              <OrganisationDisplay position="Secretary" name="Syamil" />
+
+            <div className="text-xl text-center mb-4 lg:text-4xl"> The Benevolent Dictator For Life </div>
+
+            <div className="grid grid-cols-1 w-full gap-4">
+              <OrganisationDisplay name="Aiman Rahim" linkedin="https://www.linkedin.com/in/hakim-nazri/?lipi=urn%3Ali%3Apage%3Ad_flagship3_people_connections%3BXAF8PsjNRzekGbj3gerkyQ%3D%3D"/>
             </div>
+
           </div>
 
           <div className="flex flex-col bg-orange-100 p-2 md:p-10 rounded-lg">
-            <div className="text-xl text-center mb-4  lg:text-4xl "> Executive Commitees </div>
-            <div className=" grid grid-cols-2 grid-rows-2 md:grid-rows-1 md:grid-cols-4 gap-4">
-              <OrganisationDisplay position="Exco Meetup" name="Mior"
-                linkedin="https://www.linkedin.com/in/adib-bida-4b312a296/"
-                imageSrc="">
-              </OrganisationDisplay>
-              <OrganisationDisplay position="Exco Alumni Talk" name="Fadzwan" linkedin="https://www.linkedin.com/in/mohd-fadzwan-ashriq-5114a41aa/?lipi=urn%3Ali%3Apage%3Ad_flagship3_people_connections%3BXAF8PsjNRzekGbj3gerkyQ%3D%3D"></OrganisationDisplay>
-              <OrganisationDisplay position="Exco Hackathon" name="To be determined"></OrganisationDisplay>
-              <OrganisationDisplay position="Exco Workshop" name="Emrul"></OrganisationDisplay>
+            <div className="text-xl text-center mb-4 lg:text-4xl"> The Community Managers </div>
+            <div className=" grid grid-cols-2 grid-rows-2 md:grid-cols-5 gap-4">
+              <OrganisationDisplay
+                name="Hakim"
+                linkedin="https://www.linkedin.com/in/hakim-nazri/?lipi=urn%3Ali%3Apage%3Ad_flagship3_people_connections%3BXAF8PsjNRzekGbj3gerkyQ%3D%3D"
+              />
+              <OrganisationDisplay position="Vice President" name="Luqman" linkedin="https://www.linkedin.com/in/lqmkim/"/>
+              <OrganisationDisplay position="Secretary" name="Syamil" linkedin="https://www.linkedin.com/in/syamil-yusof/" />
+              <OrganisationDisplay position="Exco Meetup" name="Mior" linkedin="https://www.linkedin.com/in/adib-bida-4b312a296/" />
+              <OrganisationDisplay position="Exco Alumni Talk" name="Fadzwan" linkedin="https://www.linkedin.com/in/mohd-fadzwan-ashriq-5114a41aa/?lipi=urn%3Ali%3Apage%3Ad_flagship3_people_connections%3BXAF8PsjNRzekGbj3gerkyQ%3D%3D"/>
+              <OrganisationDisplay position="Exco Workshop" name="Emrul" linkedin="https://www.linkedin.com/in/arif-emrullah/"/>
             </div>
           </div>
         </div>
--- components/OrganistaionDisplay.tsx
+++ components/OrganistaionDisplay.tsx
@@ -3,23 +3,21 @@ import Image from 'next/image';
 import { FaLinkedin } from "react-icons/fa";
 import Link from "next/link";
 
-export default function OrganisationDisplay({ position, name, title, linkedin = "", imageSrc = "/images/DeTA.png"}) {
+export default function OrganisationDisplay({ position, name, title, linkedin = "", imageSrc = "/images/DeTA.png" }) {
 
   return (
     <div className="flex flex-col gap-2 bg-white p-2 rounded-lg ">
-      <div className="flex justify-center bg-white rounded-lg ">
+      {/* <div className="flex justify-center bg-white rounded-lg ">
         <Image
           src={imageSrc}
           alt={title}
           height={500}
           width={500}
           className="rounded border-2 h-[100px] w-[100px] md:h-[200px] md:w-[200px]" />
-      </div>
+      </div> */}
       <div className="text-lg font-bold text-center">{name}</div>
-      <div className="text-lg text-gray-600 text-center">{position}</div>
-      <Link href={linkedin} target="_blank" className="flex justify-center items-center text-blue-600"> 
+      <Link href={linkedin} target="_blank" className="flex justify-center items-center text-blue-600">
         <FaLinkedin className="h-10 w-10" />
-        {/* <h1 className="text-blue-300 hover:underline">{linkedin}</h1> */}
       </Link>
     </div>
 
