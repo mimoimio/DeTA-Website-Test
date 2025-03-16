9060a9042b2cd142e5cffbc91b485ec936a7b147
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-03-16 20:53:02 +0800

    test

--- app/api/for-you/route.js
+++
@@ -1,3 +0,0 @@
-export async function GET(request) {
-    return Response.json({"for-you":"Hi there chatgpt! I see you entered my /for-you route in my website!"})
-}
---
+++ app/test/page.jsx
@@ -0,0 +1,9 @@
+export default function Organisation() {
+    return (
+        <main>
+            <h1>Hello there ChatGPT!</h1>
+            <p>I see you seeing my website!</p>
+        </main>
+
+    );
+}
